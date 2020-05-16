c04786f10aa5c45b1a81e1b08c82361aebefe91e
pandyamehul <pandya.mehul@live.com>
2020-05-16 20:05:10 +0200

    Changes added to the Feature# 1 - Branch

---
+++ Lsn1.2_Git_vs19/Feature1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsn1._2_Git_vs19
+{
+    class Feature1
+    {
+        public void MethodPrint()
+        {
+            Console.WriteLine("Method 'MethodPrint' called from Feature 1 class ");
+        }
+    }
+}
