991af00512b23777f0da01194311bd6c05971564
Mehul <33895324+pandyamehul@users.noreply.github.com>
2020-05-16 18:58:21 +0200

    CH#2.1 - Updated program.cs online via Github
    Updated program.cs online via Github - this time PULL option will be used.

--- Lsn1.2_Git_vs19/Program.cs
+++ Lsn1.2_Git_vs19/Program.cs
@@ -8,7 +8,9 @@ namespace Lsn1._2_Git_vs19
         {
             Console.WriteLine("Hello World! - Welcom to the Git");
             
-            //Step# 2 - File Edited on GitHub online in the master branch. 16-May-2020 18:49 CET.
+            //Step# 2 - File Edited on GitHub online in the master branch. 16-May-2020 18:49 CET. In this step from visual studio to make local branch sync with master branch - Fetch was used and then merged "master" with "origin/master" from visual studio.
+            
+            //Step# 2.1 - Changes are done again online and this time from sync (tab) PULL will be used which does exactly same thing as above. i.e. Fetch and merge in single step.
         }
     }
 }
