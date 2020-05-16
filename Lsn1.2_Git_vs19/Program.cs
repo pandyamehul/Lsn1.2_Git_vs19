40b0888d58a9954c3801c0d7f7958ad84c782831
pandyamehul <pandya.mehul@live.com>
2020-05-16 17:09:04 +0200

    ch1 - commiting theproject

---
+++ Lsn1.2_Git_vs19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lsn1._2_Git_vs19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! - Welcom to the Git");
+        }
+    }
+}
