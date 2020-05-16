48ace7ab0e93ccc00c3796a10bcfb30b25206086
Mehul <33895324+pandyamehul@users.noreply.github.com>
2020-05-16 18:50:35 +0200

    Ch#2 - Updated Program.cs online via GitHub
    Changes are done in Program.cs online via GitHub.

--- Lsn1.2_Git_vs19/Program.cs
+++ Lsn1.2_Git_vs19/Program.cs
@@ -7,6 +7,8 @@ namespace Lsn1._2_Git_vs19
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! - Welcom to the Git");
+            
+            //Step# 2 - File Edited on GitHub online in the master branch. 16-May-2020 18:49 CET.
         }
     }
 }
